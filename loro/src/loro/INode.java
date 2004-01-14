fb2e2b1de2f8b7adec438f9c4b4937c7b14bafb4
Carlos Rueda <carueda@gmail.com>
2004-01-14 09:30:28 +0000

    refactoring: new extern interfaces

---
+++ loro/src/loro/INode.java
@@ -0,0 +1,11 @@
+package loro;
+
+////////////////////////////////////////////////////////////////
+/**
+ * Interface that every node in a parse tree must implement.
+ *
+ * @author Carlos Rueda
+ * @version $Id$
+ */
+public interface INode extends IUbicable {
+}
---
+++ loro/src/loro/IUbicable.java
@@ -0,0 +1,14 @@
+package loro;
+
+/**
+ * An object that can be located in a text.
+ *
+ * @author Carlos Rueda
+ * @version $Id$
+ */
+public interface IUbicable {
+	/**
+	 * location
+	 */
+	public Rango obtRango();
+}
