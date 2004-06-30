bb1c62e58c18cfcb198ad7c38366bd809a0674cb
Carlos Rueda <carueda@gmail.com>
2004-06-30 06:21:28 +0000

    new core service

---
+++ loro/src/loro/ILanguageInfo.java
@@ -0,0 +1,10 @@
+package loro;
+
+/** Provides metainformation about the programming language. */
+public interface ILanguageInfo {
+	public String[] getKeywords1();
+	public String[] getKeywords2();
+	public String[] getKeywords3();
+	public String[] getLiterals();
+}
+
