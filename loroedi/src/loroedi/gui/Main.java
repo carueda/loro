d916148895b130d39f688ed23ffde874ee44f840
Carlos Rueda <carueda@gmail.com>
2004-07-01 09:17:34 +0000

    starting 0.9pre2 with core 0.8pre2;  using setDefaultLookAndFeelDecorated(true)

--- loroedi/src/loroedi/gui/Main.java
+++ loroedi/src/loroedi/gui/Main.java
@@ -1,28 +1,23 @@
 package loroedi.gui;
 
-//////////////////////////////////////////////////
+import javax.swing.*;
+
 /**
- * main entry point to integrated environment
- *
+ * Main entry point to integrated environment
  * @author Carlos Rueda
  */
-public class Main
-{
-	/////////////////////////////////////////////////////////////////
-	public static void main(String[] args)
-	{
+public class Main {
+	public static void main(String[] args) {
 		String prs_directory = null; // para mirar preferencia
-		
-		if ( args.length == 2 && args[0].equals("-prsdir") )
-		{
+		if ( args.length == 2 && args[0].equals("-prsdir") ) {
 			prs_directory = args[1];
 		}
-		else if ( args.length > 0 )
-		{
+		else if ( args.length > 0 ) {
 			System.out.println("\n" + "Main [-prsdir <prs-directory>]" + "\n");
 			return;
 		}
-		
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JDialog.setDefaultLookAndFeelDecorated(true);
 		GUI.init(prs_directory);
 	}
 }
